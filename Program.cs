293e9ffb33a542c0bcb8e4a082af9e25975bb013
LunaBroeke <onefivefoursakura@gmail.com>
2024-11-29 12:38:01 +0100

    Can now call to servers based on servers entered in settings.json
    First Launch sequence:
    If settings.json doesnt exist, generate settings.json.
    Close the program.
    Edit settings.json with your server address, port, JsonAPI token & optional server name.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿using Newtonsoft.Json;
 using System.Globalization;
 using System.Net.Sockets;
+using SMOOStatCheck.Settings;
+using Root = SMOOStatCheck.Root;
+using System.Text.RegularExpressions;
 
 namespace SMOOStatCheck
 {
@@ -41,7 +44,6 @@ namespace SMOOStatCheck
 
 	public class Root
 	{
-		public Settings Settings { get; set; }
 		public List<Player> Players { get; set; }
 	}
 
@@ -52,55 +54,46 @@ namespace SMOOStatCheck
 		public double Y { get; set; }
 		public double Z { get; set; }
 	}
-
-	public class Scenario
-	{
-		public bool MergeEnabled { get; set; }
-	}
-
-	public class Server
-	{
-		public int MaxPlayers { get; set; }
-	}
-
-	public class Settings
-	{
-		public PersistShines PersistShines { get; set; }
-		public Scenario Scenario { get; set; }
-		public Server Server { get; set; }
-		public Shines Shines { get; set; }
-	}
-
-	public class Shines
-	{
-		public bool Enabled { get; set; }
-	}
 	internal class Program
 	{
-		static void Main(string[] args)
+		public Settings.Settings settings = Settings.Settings.LoadSettings();
+		static void Main(string[] args) => new Program().Start();
+
+		private async void Start()
 		{
-			Log(RequestRawJson("luwuna.smoo.it", 1027));
-			Thread server1 = new Thread(() => { Log(RequestRawJson("luwuna.smoo.it", 1027)); }) { IsBackground = true };
-			Thread server2 = new Thread(() => { Log(RequestRawJson("luwuna.smoo.it", 1028)); }) { IsBackground = true };
-			Thread server3 = new Thread(() => { Log(RequestRawJson("luwuna.smoo.it", 1029)); }) { IsBackground = true };
-			server1.Start();
-			server2.Start();
-			server3.Start();
+			foreach(Server server in settings.root.Servers)
+			{
+				Thread t = new Thread(() => 
+				{
+					while (true)
+					{
+						Root r = RequestData(server.Adress, server.Port, server.Token);
+						if (r == null) { return; }
+						Log($"Player Count: {r.Players.Count} - {server}, Requesting again in {settings.root.IntervalSeconds}");
+						Thread.Sleep(settings.root.IntervalSeconds*1000);
+					}
+				});
+				t.Start();
+			}
 			Console.ReadKey();
 		}
 
 		public static void Log(string message)
 		{
-			Console.WriteLine(message);
+			string time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+			Console.WriteLine($"[{time}]{message}");
 		}
 
-		public static string RequestRawJson(string address, int port) 
+		public static string RequestRawJson(string address, int port, string token) 
 		{
+			if (address == string.Empty) { Log("Address missing"); return string.Empty; }
+			if (port == 0) { Log("Port missing"); return string.Empty; }
+			if (token == string.Empty) { Log("Token missing"); return string.Empty; }
 			RequestRoot root = new RequestRoot()
 			{
 				API_JSON_REQUEST = new APIRequest()
 				{
-					Token = AuthTokens.SMOOtoken,
+					Token = token
 				}
 			};
 			string json = JsonConvert.SerializeObject(root);
@@ -133,6 +126,50 @@ namespace SMOOStatCheck
 			{
 				Log($"failed calling {address}:{port}");
 				return string.Empty;
+			}
+		}
+		public static Root RequestData(string address, int port, string token)
+		{
+			if (address == string.Empty) { Log("Address missing"); return null; }
+			if (port == 0) { Log("Port missing"); return null; }
+			if (token == string.Empty) { Log("Token missing"); return null; }
+			RequestRoot root = new RequestRoot()
+			{
+				API_JSON_REQUEST = new APIRequest()
+				{
+					Token = token
+				}
+			};
+			string json = JsonConvert.SerializeObject(root);
+			Log(json);
+			string response = string.Empty;
+			try
+			{
+				using (var client = new TcpClient())
+				{
+					client.ReceiveTimeout = 500;
+					client.SendTimeout = 500;
+					var result = client.BeginConnect(address, port, null, null);
+					bool success = result.AsyncWaitHandle.WaitOne(TimeSpan.FromSeconds(2));
+					if (!success)
+					{
+						throw new SocketException();
+					}
+					using (var stream = client.GetStream())
+					using (var writer = new StreamWriter(stream))
+					using (var reader = new StreamReader(stream))
+					{
+						writer.Write(json);
+						writer.Flush();
+						response = reader.ReadToEnd();
+						return JsonConvert.DeserializeObject<Root>(response);
+					}
+				}
+			}
+			catch
+			{
+				Log($"failed calling {address}:{port}");
+				return null;
 			}
 		}
 		private class RequestRoot
---
+++ Settings.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMOOStatCheck.Settings
+{
+	public class Root
+	{
+		public int IntervalSeconds { get; set; } = 300;
+		public List<Server> Servers { get; set; } = new List<Server>();
+	}
+	public class Server
+	{
+		public string Name { get; set; } = "server";
+		public string Adress { get; set; } = "127.0.0.1";
+		public int Port { get; set; } = 1027;
+		public string Token { get; set; } = string.Empty;
+
+		public override string ToString()
+		{
+			return $"{Name} - {Adress}:{Port} (Token: {Token})";
+		}
+	}
+	public class Settings
+	{
+		private const string path = "settings.json";
+		public Root root = new Root();
+
+		public static Settings LoadSettings()
+		{
+			if (File.Exists(path))
+			{
+				Root _root = JsonConvert.DeserializeObject<Root>(File.ReadAllText(path));
+				Program.Log($"Loaded {path}");
+				File.WriteAllText(path, JsonConvert.SerializeObject(_root,Formatting.Indented));
+				return new Settings { root = _root };
+			}
+			else
+			{
+				Root _root = new Root();
+				_root.Servers.Add(new Server());
+				string json = JsonConvert.SerializeObject(_root, Formatting.Indented);
+				File.WriteAllText(path, json);
+				Program.Log($"Created {path} at {Path.GetFullPath(path)} \n Please remember to edit this file with your servers and reopen the program.");
+				return new Settings { root = _root };
+			}
+		}
+	}
+}
